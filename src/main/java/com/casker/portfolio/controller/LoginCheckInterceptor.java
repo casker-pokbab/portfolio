1cdc622eed926e1ee435f78744ced99ec1b9e5f1
kanghoon.choi <kanghoon.choi@nhnent.com>
2015-10-27 01:15:16 +0900

    #27 로그인 인터셉터 구현

--- src/main/java/com/casker/portfolio/controller/LoginCheckInterceptor.java
+++ src/main/java/com/casker/portfolio/controller/LoginCheckInterceptor.java
@@ -2,19 +2,22 @@ package com.casker.portfolio.controller;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.casker.portfolio.domain.User;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String id = (String) request.getSession().getAttribute("id");
-        if(id == null || id.length() == 0) {
-            response.sendRedirect("/login");
-            return false;
-        }
 
-        return super.preHandle(request, response, handler);
-    }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		User user = (User)request.getSession().getAttribute("user");
+		if (user == null) {
+			response.sendRedirect("/admin/loginForm");
+			return false;
+		}
+
+		return super.preHandle(request, response, handler);
+	}
 
 }
