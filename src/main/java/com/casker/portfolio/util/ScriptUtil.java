56568d033708f952d223a6cbced1ba5c7643af66
kanghoon.choi <kanghoon.choi@nhnent.com>
2015-11-23 20:37:53 +0900

    #64 인스타 링크 주소 변경

--- src/main/java/com/casker/portfolio/util/ScriptUtil.java
+++ src/main/java/com/casker/portfolio/util/ScriptUtil.java
@@ -16,18 +16,22 @@ public class ScriptUtil {
 	private ScriptUtil() {	}
 	
 	public static String alert(String message) {
-		return appendScriptTag("alert(\"" + message + "\");");
+		return appendScriptTag("alert('" + message + "');");
 	}
 	
 	public static String alertAndBack(String message) {
-		return appendScriptTag("alert(\"" + message + "\");history.back();");
+		return appendScriptTag("alert('" + message + "');history.back();");
 	}
 	
 	public static String alertAndRedirect(String message, String url) {
-		return appendScriptTag("alert(\"" + message + "\");location.href=\"" + url + "\";");
+		return appendScriptTag("alert('" + message + "');location.href=\"" + url + "\";");
+	}
+	
+	public static String alertAndLoginPopup(String message, String url) {
+		return appendScriptTag("alert('" + message + "');window.open('" + url + "','','width=800, height=600, resizable=no, scrollbars=no, status=no;').focus();");
 	}
 	
 	private static String appendScriptTag(String script) {
-		return "<html><script>" + script + "</script>";
+		return "<html><script>" + script + "</script></html>";
 	}
 }
